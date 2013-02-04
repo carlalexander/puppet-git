8e9cb57378da541756819d9656a05ca7910e4de0
Carl Alexander <carlalexander@gmail.com>
2013-02-04 12:12:16 -0500

    Cannot configure server wide

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,16 +17,6 @@
 # node default {
 #   include git
 # }
-class git (
-  $user_name         = undef,
-  $user_email        = undef,
-  $credential_helper = undef,
-) {
+class git {
   class { 'git::package': }
-
-  class { 'git::config':
-    user_name         => $user_name,
-    user_email        => $user_email,
-    credential_helper => $credential_helper,
-  }
 }
